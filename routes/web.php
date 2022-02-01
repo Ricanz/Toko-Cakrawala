336cfcd73e6beb74c258ebf397ca812750914ac6
erza <58721335+erzaffasya@users.noreply.github.com>
2022-02-01 20:42:53 +0700

    setting template

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/landingPage', function () {
-    return view('welcome');
+Route::get('/test', function () {
+    return view('index');
 });
 
-Route::get('/admin', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
