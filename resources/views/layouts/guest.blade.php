02b95d8005cd64bbd01c496904cb509e754c7334
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-02-15 13:05:54 +0700

    up

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,13 +13,17 @@
     {{-- <div id="ec-overlay"><span class="loader_img"></span></div> --}}
 
     <!-- Header start  -->
-    @include('partials.header')
+    @include('partials.navbar')
     <!-- Header End  -->
 
     <!-- Ekka Cart Start -->
+    @include('partials.side-cart')
+    <!-- Ekka Cart End -->
+
+    <main>
+        {{ $slot }}
+    </main>
     
-    {{ $slot }}
-        
     <!-- Footer Start -->
     @include('partials.footer')
     <!-- Footer Area End -->
@@ -35,36 +39,36 @@
                             <!-- Swiper -->
                             <div class="qty-product-cover">
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_1.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_1.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_2.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/93_1.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/93_1.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/93_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/93_2.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_2.jpg" alt="">
                                 </div>
                             </div>
                             <div class="qty-nav-thumb">
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_1.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_1.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_2.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/93_1.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/93_1.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/93_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/93_2.jpg" alt="">
                                 </div>
                                 <div class="qty-slide">
-                                    <img class="img-responsive" src="{{asset('tlandingPage/assets/images/product-image/94_2.jpg')}}" alt="">
+                                    <img class="img-responsive" src="assets/images/product-image/94_2.jpg" alt="">
                                 </div>
                             </div>
                         </div>
@@ -118,104 +122,55 @@
     <!-- Modal end -->
 
     <!-- Click To Call -->
-    <div class="ec-cc-style cc-right-bottom">
-        <!-- Start Floating Panel Container -->
-        <div class="cc-panel">			
-            <!-- Panel Content -->
-            <div class="cc-header">
-                <img src="{{asset('tlandingPage/assets/images/whatsapp/profile_01.jpg')}}" alt="profile image"/>
-                <h2>John Mark</h2>
-                <p>Tachnical Manager</p>
-            </div>
-            <div class="cc-body">
-                <p><b>Hey there &#128515;</b></p>
-                <p>Need help ? just give me a call.</p>
-            </div>
-            <div class="cc-footer">
-                <a href="tel:+919099153528" class="cc-call-button">
-                    <span>Call me</span>
-                    <svg width="13px" height="10px" viewBox="0 0 13 10">
-                    <path d="M1,5 L11,5"></path>
-                    <polyline points="8 1 12 5 8 9"></polyline>
-                    </svg>
-                </a>
-            </div>
-        </div>
-        <!--/ End Floating Panel Container -->
-
-        <!-- Start Right Floating Button-->
-        <div class="cc-button cc-right-bottom">
-            <img src="{{asset('tlandingPage/assets/images/icons/call.svg')}}" class="svg_img cc-call-svg" alt="call image" />
-        </div>
-        <!--/ End Right Floating Button-->
-
-    </div>
+    @include('partials.cc')
     <!-- Click To Call end -->
 
-    {{-- <!-- Newsletter Modal Start -->
-    <div id="ec-popnews-bg"></div>
-    <div id="ec-popnews-box">
-        <div id="ec-popnews-close"><i class="ecicon eci-close"></i></div>
-        <div class="row">
-            <div class="col-md-7 disp-no-767">
-                <img src="{{asset('tlandingPage/assets/images/banner/newsletter-8.png')}}" alt="newsletter">
-            </div>
-            <div class="col-md-5">
-                <div id="ec-popnews-box-content">
-                    <h2>Subscribe Newsletter.</h2>
-                    <p>Subscribe the ekka ecommerce to get in touch and get the future update. </p>
-                    <form id="ec-popnews-form" action="#" method="post">
-                        <input type="email" name="newsemail" placeholder="Email Address" required />
-                        <button type="button" class="btn btn-primary" name="subscribe">Subscribe</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+    <!-- Newsletter Modal Start -->
+    {{-- @include('partials.newsletter') --}}
     <!-- Newsletter Modal end -->
     
     <!-- Footer navigation panel for responsive display -->
-    {{-- <div class="ec-nav-toolbar">
+    <div class="ec-nav-toolbar">
         <div class="container">
             <div class="ec-nav-panel">
                 <div class="ec-nav-panel-icons">
                     <a href="#ec-mobile-menu" class="navbar-toggler-btn ec-header-btn ec-side-toggle"><img
-                            src="{{asset('tlandingPage/assets/images/icons/menu.svg')}}" class="svg_img header_svg" alt="icon" /></a>
+                            src="assets/images/icons/menu.svg" class="svg_img header_svg" alt="icon" /></a>
                 </div>
                 <div class="ec-nav-panel-icons">
                     <a href="#ec-side-cart" class="toggle-cart ec-header-btn ec-side-toggle"><img
-                            src="{{asset('tlandingPage/assets/images/icons/cart.svg')}}" class="svg_img header_svg" alt="icon" /><span
+                            src="assets/images/icons/cart.svg" class="svg_img header_svg" alt="icon" /><span
                             class="ec-cart-noti ec-header-count ec-cart-count cart-count-lable">3</span></a>
                 </div>
                 <div class="ec-nav-panel-icons">
-                    <a href="index.html" class="ec-header-btn"><img src="{{asset('tlandingPage/assets/images/icons/home.svg')}}"
+                    <a href="index.html" class="ec-header-btn"><img src="assets/images/icons/home.svg"
                             class="svg_img header_svg" alt="icon" /></a>
                 </div>
                 <div class="ec-nav-panel-icons">
-                    <a href="wishlist.html" class="ec-header-btn"><img src="{{asset('tlandingPage/assets/images/icons/wishlist.svg')}}"
+                    <a href="wishlist.html" class="ec-header-btn"><img src="assets/images/icons/wishlist.svg"
                             class="svg_img header_svg" alt="icon" /><span class="ec-cart-noti">4</span></a>
                 </div>
                 <div class="ec-nav-panel-icons">
                     <a href="#ec-mobile-sidebar" class="ec-header-btn ec-sidebar-toggle d-lg-none">
-                        <img src="{{asset('tlandingPage/assets/images/icons/category-icon.svg')}}" class="svg_img header_svg" alt="icon" />
+                        <img src="assets/images/icons/category-icon.svg" class="svg_img header_svg" alt="icon" />
                     </a>
                 </div>
 
             </div>
         </div>
-    </div> --}}
+    </div>
     <!-- Footer navigation panel for responsive display end -->
 
     <!-- Recent Purchase Popup  -->
-    <div class="recent-purchase">
-        <img src="{{asset('tlandingPage/assets/images/product-image/111_1.jpg')}}" alt="payment image">
+    {{-- <div class="recent-purchase">
+        <img src="assets/images/product-image/111_1.jpg" alt="payment image">
         <div class="detail">
             <p>Someone in new just bought</p>
             <h6>Rose Gold Earrings</h6>
             <p>2 Minutes ago</p>
         </div>
         <a href="javascript:void(0)" class="icon-btn recent-close">×</a>
-    </div>
+    </div> --}}
     <!-- Recent Purchase Popup end -->
 
     <!-- Add to Cart successfully toast Start -->
@@ -227,6 +182,7 @@
     </div>
     <!-- Add to Cart successfully toast end -->
 
+    {{-- Java Script --}}
     @include('partials.scripts')
 </body>
 
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,39 +3,31 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     
-    <title>Toko Cakrawala - Tagline Cakrawala</title>
+    <title>Toko Cakrawala - Landing Page</title>
     <meta name="keywords" content="apparel, catalog, clean, ecommerce, ecommerce HTML, electronics, fashion, html eCommerce, html store, minimal, multipurpose, multipurpose ecommerce, online store, responsive ecommerce template, shops" />
-    <meta name="description" content="Best ecommerce html template for single and multi vendor store.">
-    <meta name="author" content="ashishmaraviya">
+    <meta name="description" content="Toko barang terbaik di Indonesia">
+    <meta name="author" content="nikibweb">
     
    <!-- site Favicon -->
-   <link rel="icon" href="{{asset("tlandingPage/assets/images/favicon/favicon-8.png")}}" sizes="32x32" />
-   <link rel="apple-touch-icon" href="{{asset("tlandingPage/assets/images/favicon/favicon-8.png")}}" />
-   <meta name="msapplication-TileImage" content="{{asset("tlandingPage/assets/images/favicon/favicon-8.png")}}" />
+   <link rel="icon" href="{{asset('tlandingPage/assets/images/favicon/favicon-8.png')}}" sizes="32x32" />
+   <link rel="apple-touch-icon" href="{{asset('tlandingPage/assets/images/favicon/favicon-8.png')}}" />
+   <meta name="msapplication-TileImage" content="{{asset('tlandingPage/assets/images/favicon/favicon-8.png')}}" />
 
    <!-- css Icon Font -->
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/vendor/ecicons.min.css")}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/vendor/ecicons.min.css')}}" />
 
    <!-- css All Plugins Files -->
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/animate.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/swiper-bundle.min.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/jquery-ui.min.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/countdownTimer.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/nouislider.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/slick.min.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/owl.carousel.min.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/owl.theme.default.min.css")}}" />
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/plugins/bootstrap.css")}}" />
-   
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/animate.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/swiper-bundle.min.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/jquery-ui.min.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/countdownTimer.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/nouislider.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/slick.min.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/owl.carousel.min.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/owl.theme.default.min.css')}}" />
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/plugins/bootstrap.css')}}" />
 
    <!-- Main Style -->
-   <link rel="stylesheet" href="{{asset("tlandingPage/assets/css/demo8.css")}}" />
-
+   <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/demo8.css')}}" />
    
-    <!-- Main Style -->
-    <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/style.css')}}" />
-    <link rel="stylesheet" href="{{asset('tlandingPage/assets/css/responsive.css')}}" />
-
-    <!-- Background css -->
-    <link rel="stylesheet" id="bg-switcher-css" href="{{asset('tlandingPage/assets/css/backgrounds/bg-4.css')}}">
 </head>
