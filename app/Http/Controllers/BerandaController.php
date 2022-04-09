ac26aab2e161e265ee47e34697d5a200051ea746
ctriyanti <riyanti@cashtree.id>
2022-04-09 14:56:14 +0700

    banner index view

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Helpers\Helper;
 use App\Models\Artikel;
+use App\Models\Banner;
 use App\Models\Produk;
 use App\Models\Kategori;
 use App\Models\subKategori;
@@ -20,12 +21,13 @@ class BerandaController extends Controller
         $kategori = Kategori::all();
         $testimonial = Testimonial::all();
         $artikel = Artikel::all();
+        $banner = Banner::where('status', 'aktif')->get();
         // dd(response()->json(request()->cookie('shopping_cart')));
         // dd(Helper::getCart());  
         // foreach(Helper::getCart() as $key => $item){
         //     dd($item);
         // }
-        return view('index', compact('produk', 'kategori', 'produkselect', 'testimonial', 'artikel'));
+        return view('index', compact('produk', 'kategori', 'produkselect', 'testimonial', 'artikel', 'banner'));
     }
 
     public function katalog(){
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,18 @@
             <div class="ec-slider swiper-container main-slider-nav main-slider-dot">
                 <!-- Main slider -->
                 <div class="swiper-wrapper">
-                    <div class="ec-slide-item swiper-slide d-flex slide-1">
+
+                    @foreach ($banner as $item)
+                    <div class="ec-slide-item swiper-slide d-flex slide-{{ $item->urutan }}">
                         <div class="container align-self-center">
                             <div class="row">
                                 <div class="col-sm-12 align-self-center">
                                     <div class="ec-slide-content slider-animation">
-                                        <h2 class="ec-slide-stitle">Sale offer</h2>
-                                        <h1 class="ec-slide-title">New Fashion Summer Sale</h1>
+                                        <h2 class="ec-slide-stitle">{{ $item->sub_judul }}</h2>
+                                        <h1 class="ec-slide-title">{{ $item->judul }}</h1>
                                         <div class="ec-slide-desc">
-                                            <p>starting at $ <b>29</b>.99</p>
-                                            <a href="#" class="btn btn-lg btn-primary">Shop Now <i
+                                            <p>{{ $item->deskripsi }}</p>
+                                            <a href="{{ $item->link }}" class="btn btn-lg btn-primary">{{ $item->tombol }}<i
                                                     class="ecicon eci-angle-double-right" aria-hidden="true"></i></a>
                                         </div>
                                     </div>
@@ -23,40 +25,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="ec-slide-item swiper-slide d-flex slide-2">
-                        <div class="container align-self-center">
-                            <div class="row">
-                                <div class="col-sm-12 align-self-center">
-                                    <div class="ec-slide-content slider-animation">
-                                        <h2 class="ec-slide-stitle">Trending item</h2>
-                                        <h1 class="ec-slide-title">Women's latest fashion sale</h1>
-                                        <div class="ec-slide-desc">
-                                            <p>starting at $ <b>20</b>.00</p>
-                                            <a href="#" class="btn btn-lg btn-primary">Shop Now <i
-                                                    class="ecicon eci-angle-double-right" aria-hidden="true"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ec-slide-item swiper-slide d-flex slide-3">
-                        <div class="container align-self-center">
-                            <div class="row">
-                                <div class="col-sm-12 align-self-center">
-                                    <div class="ec-slide-content slider-animation">
-                                        <h2 class="ec-slide-stitle">Trending accessories</h2>
-                                        <h1 class="ec-slide-title">Modern Sunglasses</h1>
-                                        <div class="ec-slide-desc">
-                                            <p>starting at $ <b>15</b>.00</p>
-                                            <a href="#" class="btn btn-lg btn-primary">Shop Now <i
-                                                    class="ecicon eci-angle-double-right" aria-hidden="true"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="swiper-pagination swiper-pagination-white"></div>
                 <div class="swiper-buttons">
