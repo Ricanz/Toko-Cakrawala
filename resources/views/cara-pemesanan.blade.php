ae8d219ff29d540ec84b7fe59fc36ded4f152a12
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-05-15 10:17:02 +0700

    push

--- resources/views/cara-pemesanan.blade.php
+++ resources/views/cara-pemesanan.blade.php
@@ -4,47 +4,6 @@
     <div class="container">
         <div class="row">
 
-            <!-- Sidebar area start -->
-            <div class="ec-side-cat-overlay"></div>
-            <div class="col-lg-3 sidebar-dis-991" data-animation="fadeIn">
-                <div class="cat-sidebar">
-                    <div class="cat-sidebar-box">
-                        <div class="ec-sidebar-wrap">
-                            <!-- Sidebar Category Block -->
-                            <div class="ec-sidebar-block">
-                                <div class="ec-sb-title">
-                                    <h3 class="ec-sidebar-title">Kategori<button class="ec-close">×</button>
-                                    </h3>
-                                </div>
-                                @foreach ($kategori as $k)
-                                    <div class="ec-sb-block-content">
-                                        <ul>
-                                            <li>
-                                                <div class="ec-sidebar-block-item"><img
-                                                        src="{{ asset('tlandingPage/assets/images/icons/dress-8.svg') }}"
-                                                        class="svg_img" alt="drink" />{{ $k->nama }}
-                                                </div>
-                                                <ul style="display: block;">
-                                                    @foreach ($k->subkategori as $i)
-                                                        <li>
-                                                            <div class="ec-sidebar-sub-item"><a
-                                                                    href="#">{{ $i->nama }}<span
-                                                                        title="Available Stock"></span></a>
-                                                            </div>
-                                                        </li>
-                                                    @endforeach
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                @endforeach
-                            </div>
-                            <!-- Sidebar Category Block -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-
             <!-- Product area start -->
             <div class="col-lg-9 col-md-12">
 
